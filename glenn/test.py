274733b58d39552957d4a32bb409c69f33ca3fa6
glenn mitchell <glenn.mitchell@outlook.com>
2021-05-01 08:34:32 +0000

    Updates

--- glenn/test.py
+++ glenn/test.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "It works!!"
+    return "Yay! It works!!"
 
 port = int(os.environ.get('PORT', 84))
 app.run(host='0.0.0.0', port=port)
