13480b8041957db620578d557cbe1dbb4b5520c7
Glenn Mitchell <glenn.mitchell@outlook.com>
2021-05-11 12:49:42 +1000

    Patrice's demo files

--- demo/app.py
+++ demo/app.py
@@ -9,32 +9,32 @@ app = Flask(__name__)
 
 # SPINNER ROUTE
 @app.route('/spinner')
-# create spinner method - this will be run when you go to /spinner
+# this is a method!
 def spinner():
     drink_data = getRandomDrink()
-    drink_object = extractDrinkData(drink_data)
-    # return text to user
-    return render_template('spinner.html', drink_object=drink_object)
+    drink_obj = extractDrinkData(drink_data)
+    # display html to the user
+    return render_template('spinner.html', drink_obj=drink_obj)
 
 def getRandomDrink():
     random_drink_data = requests.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-    # load as json
+    # tell python the data is in json
     json_object = json.loads(random_drink_data.text)
-    # extract 1st drink in list
-    drink_data = json_object["drinks"][0]
-    return drink_data
+    # extract 1st drink in the list
+    data = json_object["drinks"][0]
+    return data
 
-def extractDrinkData(drink_data):
+def extractDrinkData(data):
     # get name
-    name = drink_data["strDrink"]
-    img = drink_data['strDrinkThumb'] + '/preview'
-    instructions = drink_data['strInstructions']
-    ingredient_1 = drink_data['strIngredient1']
-    drink_object = {'name': name, 'img': img, 'instructions': instructions, 'ingredient': ingredient_1}
+    name = data["strDrink"]
+    instructions = data["strInstructions"]
+    ingredient_1 = data['strIngredient1']
+    image = data['strDrinkThumb'] + '/preview'
+    # create a drink objects
+    drink_object = {'drink_name': name, 'drink_img': image, 'instructions': instructions, 'ingredient': ingredient_1}
     return drink_object
-    
 
 # RUN APP
-# run app on 82 port - binding to host 0.0.0.0
+# run the flask application - we can specify a port number
 port = int(os.environ.get('PORT', 82))
 app.run(host='0.0.0.0', port=port)
